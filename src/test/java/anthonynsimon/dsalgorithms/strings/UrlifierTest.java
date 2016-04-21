60c0ccf923a2488c3a8e9b0d72c7098f60a0208d
Anthony N. Simon <anthonynsimon@gmail.com>
2016-04-21 17:05:38 +0200

    added URLify algorithm

---
+++ src/test/java/anthonynsimon/dsalgorithms/strings/UrlifierTest.java
@@ -0,0 +1,24 @@
+package anthonynsimon.dsalgorithms.strings;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UrlifierTest {
+  
+  private Urlifier classUnderTest = new Urlifier();
+  
+  @Test
+  public void testReplaceWhitespace() {
+    assertEquals("the%20dog%20is%20happy", 
+        classUnderTest.urlify("the dog is happy"));
+    
+    assertEquals("google.com/q?search=java%20data%20structures%20And%20algorithms", 
+        classUnderTest.urlify("google.com/q?search=java data structures And algorithms"));
+        
+    assertEquals("http://google.com/asd%20tta%20%20asd", classUnderTest.urlify("http://google.com/asd tta  asd"));
+        
+    assertEquals("http://google.com/", classUnderTest.urlify("http://google.com/"));
+        
+    assertEquals("http://google.com/q?%20%20%20%20%20test%20%20", classUnderTest.urlify("http://google.com/q?     test  "));
+  }
+}
