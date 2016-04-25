0dc17b98d04de91858c6c1468b26d1a1dc4d6743
Anthony N. Simon <anthonynsimon@gmail.com>
2016-04-25 11:24:42 +0200

    added string compression

---
+++ src/test/java/anthonynsimon/dsalgorithms/strings/StringCompressorTest.java
@@ -0,0 +1,32 @@
+package anthonynsimon.dsalgorithms.strings;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringCompressorTest {
+  
+  private StringCompressor classUnderTest = new StringCompressor();
+  
+  @Test
+  public void testEmptyString() {
+    assertEquals(classUnderTest.compress(""), "");
+  }
+  
+  @Test
+  public void testNoCompressionNeeded() {
+    assertEquals(classUnderTest.compress("abcdefg"), "abcdefg");
+  }
+  
+  @Test
+  public void testCompressionWorseCase() {
+    // Result would actually be "a2b1" which is longer than original
+    assertEquals(classUnderTest.compress("aab"), "aab");
+  }
+  
+  @Test
+  public void testCompressionNeeded() {
+    assertEquals(classUnderTest.compress("aaabcbbbaaccccccgxgg"), "a3b1c1b3a2c6g1x1g2");
+  }
+  
+
+}
