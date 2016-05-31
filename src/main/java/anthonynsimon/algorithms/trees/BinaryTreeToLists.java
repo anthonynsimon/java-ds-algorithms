502f444671dd22bf7fb63c45113f6b81c2e75080
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-31 11:52:51 +0200

    added comments

--- src/main/java/anthonynsimon/algorithms/trees/BinaryTreeToLists.java
+++ src/main/java/anthonynsimon/algorithms/trees/BinaryTreeToLists.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class BinaryTreeToLists<T> {
   
+  // Collects the Tree nodes per level into a list and returns them list of lists
   public ArrayList<ArrayList<BinaryNode<T>>> treeToListsByLevel(BinarySearchTree tree) {
     ArrayList<ArrayList<BinaryNode<T>>> results = new ArrayList<>();
     
@@ -14,19 +15,23 @@ public class BinaryTreeToLists<T> {
       return results;
     }
     
+    // Setup for Breadth first search
     Queue<BinaryNode<T>> queue = new Queue<>();
     
     queue.enqueue(tree.getRootNode());
     
+    // Traverse the tree per level using BFS,
     while (!queue.isEmpty()) {
       ArrayList<BinaryNode<T>> level = new ArrayList<>();
       
+      // Get the level's nodes from the queue
       while (!queue.isEmpty()) {
         level.add(queue.dequeue());
       }
       
       results.add(level);
       
+      // Enqueue the children of the current level's nodes if any
       for (BinaryNode<T> node : level) {
         if (node.hasLeftChild()) {
           queue.enqueue(node.getLeft());
