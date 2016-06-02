12d5eee381f2f6e655de50590c8c349deeb88014
Anthony N. Simon <anthonynsimon@gmail.com>
2016-06-02 16:04:31 +0200

    switched to Hoare partition scheme

--- src/main/java/anthonynsimon/algorithms/sorts/QuickSort.java
+++ src/main/java/anthonynsimon/algorithms/sorts/QuickSort.java
@@ -3,52 +3,56 @@ package anthonynsimon.algorithms.sorts;
 import java.util.Arrays;
 
 public class QuickSort {
-  
+    
   public void sort(int[] array) {
     if (array.length < 2) {
       return;
     }
    
     quickSort(array, 0, array.length - 1);
+    System.out.println(swaps);
   }
   
   private void quickSort(int[] array, int min, int max) {
     // Base case, partition is of size one or null
-    if (min >= max) {
+    if (min > max) {
       return;
     }
     
-    // Set starting pivot at last position
-    int pivot = max;
-    int left = min;
-    int right = max;
-    
-    // Repeat until pointers meet
-    while (left < right) {
-      // Continue until left side swap candidate is found
-      while (array[left] < array[pivot]) {
-        left++;
-      }
-      
-      // Swap and update pivot index
-      swap(array, left, pivot);
-      pivot = left;
-      
-      // Continue until right side swap candidate is found
-      while (array[right] > array[pivot]) {
-        right--;
-      }
-      
-      // Swap and update pivot index
-      swap(array, right, pivot);
-      pivot = right;
-    }
+    // Reorder partition so that x < pivot on the left and x >= pivot on the right
+    // Returned int is the index of the pivot
+    int pivot = partition(array, min, max);
     
     // Recursively apply quick sort to left and right side of list
     quickSort(array, min, pivot - 1);
     quickSort(array, pivot + 1, max);
   }
+  
+  private int partition(int[] array, int min, int max) {
+    int pivot = array[max];
+    int left = min;
+    int right = max;
     
+    // Repeat until pointers meet
+    while (true) {
+      // Keep moving left pointer until swap candidate is found
+      while (array[left] < pivot) {
+        left++;
+      }
+      // Same for right pointer
+      while (array[right] > pivot) {
+        right--;
+      }
+      
+      // If pointers met, we're done on this partition, pivot is on right pointer
+      if (left >= right) {
+        return right;
+      }
+      
+      // Swap elements
+      swap(array, left, right);
+    }
+  }
   
   private void swap(int[] array, int a, int b) {
     int temp = array[a];
