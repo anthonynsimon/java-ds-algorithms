78c341e2a072707cd8d5b18dc3ad06a0ec9becc4
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-06-18 18:31:02 +0200

    added comments to trie

--- src/main/java/anthonynsimon/datastructures/Trie.java
+++ src/main/java/anthonynsimon/datastructures/Trie.java
@@ -16,9 +16,12 @@ public class Trie<T> {
     if (word == null || word.length() <= 0) {
       return;
     }
-
+    // For simplicity, treat everything as lowercase
     word = word.toLowerCase();
 
+    // Starting from the root, find the right slot for each
+    // charater in the word or create a new node if not initialized.
+    // Once last character is reached, set last slot's value.
     TrieNode<T> current = this.root;
     for (int i = 0; i < word.length(); i++) {
       int index = charIndexAt(word, i);
@@ -34,6 +37,7 @@ public class Trie<T> {
     if (word == null || word.length() <= 0) {
       return;
     }
+    // Start the recursive call to remove the word if present
     removeWorker(word, this.root, 0);
   }
 
@@ -74,22 +78,29 @@ public class Trie<T> {
   }
 
   protected void removeWorker(String word, TrieNode node, int i) {
+    // Base case, we reached the end of the word
     if (i >= word.length()) {
       return;
     }
-
+    
+    // Map the ASCII char index to 0-25 range
     int charIndex = charIndexAt(word, i);
 
+    // If no node for current char, then word has not been placed in trie
     if (node.children[charIndex] == null) {
       return;
     }
 
+    // If node exists then start recursive call on it for the next char
     removeWorker(word, node.children[charIndex], i + 1);
 
+    // Once we return from the recursive call, if we are at the end of the 
+    // word then start the removal by setting the value to null
     if (i == word.length() - 1) {
       node.children[charIndex].value = null;
     }
 
+    // if the current node is empty, then set it to null and propagate change up
     if (node.children[charIndex].isEmpty()) {
       node.children[charIndex] = null;
     }
