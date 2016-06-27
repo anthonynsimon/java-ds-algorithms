bb520226cd48c643aefc10b85f29826eb6810424
Anthony N. Simon <anthonynsimon@gmail.com>
2016-06-27 10:20:21 +0200

    cleaned up code

--- src/main/java/anthonynsimon/datastructures/PriorityQueue.java
+++ src/main/java/anthonynsimon/datastructures/PriorityQueue.java
@@ -2,14 +2,11 @@ package anthonynsimon.datastructures;
 
 import anthonynsimon.datastructures.util.HeapNode;
 
-public class PriorityQueue<T extends Comparable<T>> {
+public class PriorityQueue<T> {
 
   protected HeapNode[] heap;
   protected int size;
-  protected int initialCapacity;
-  protected int capacity;
-  protected int GROWTH_FACTOR = 2;
-  protected int offset = 1;
+  protected int maxCapacity;
 
   public PriorityQueue() {
     this(256);
@@ -22,16 +19,18 @@ public class PriorityQueue<T extends Comparable<T>> {
     }
   }
 
-  public PriorityQueue(int initialCapacity) {
-    this.initialCapacity = initialCapacity;
-    capacity = initialCapacity;
-    heap = new HeapNode[capacity];
+  public PriorityQueue(int maxCapacity) {
+    this.maxCapacity = maxCapacity;
+    heap = new HeapNode[maxCapacity];
     size = 0;
   }
 
   public void insert(int priority, T item) {
-    ensureCapacity();
-    heap[getNextAvailableIndex()] = new HeapNode<T>(priority, item);
+    if (size() >= maxCapacity) {
+      return;
+    }
+
+    heap[size()] = new HeapNode<T>(priority, item);
     size++;
     percolateUp(getIndexAtLast());
   }
@@ -40,7 +39,7 @@ public class PriorityQueue<T extends Comparable<T>> {
     if (size() <= 0) {
       return null;
     }
-    return (T)heap[getIndexAt(0)].data;
+    return (T)heap[0].data;
   }
 
   public T removeMin() {
@@ -48,20 +47,24 @@ public class PriorityQueue<T extends Comparable<T>> {
       return null;
     }
 
-    HeapNode min = heap[getIndexAt(0)];
+    HeapNode min = heap[0];
     if (size() == 1) {
-      heap[getIndexAt(0)] = null;
+      heap[0] = null;
       size--;
     }
     else {
       HeapNode last = heap[getIndexAtLast()];
       heap[getIndexAtLast()] = null;
       size--;
-      heap[getIndexAt(0)] = last;
-      percolateDown(getIndexAt(0));
+      heap[0] = last;
+      percolateDown(0);
     }
 
     return min != null ? (T)min.data : null;
+  }
+
+  public int size() {
+    return size;
   }
 
   public boolean isEmpty() {
@@ -69,14 +72,14 @@ public class PriorityQueue<T extends Comparable<T>> {
   }
 
   public void clear() {
-    heap = new HeapNode[capacity];
+    heap = new HeapNode[maxCapacity];
     size = 0;
   }
 
-  public Comparable[] toArray() {
-    Comparable[] result = new Comparable[size];
+  public T[] toArray() {
+    T[] result = (T[]) new Object[size];
     for (int i = 0; i < size; i++) {
-      result[i] = (T)heap[getIndexAt(i)].data;
+      result[i] = (T)heap[i].data;
     }
     return result;
   }
@@ -84,7 +87,7 @@ public class PriorityQueue<T extends Comparable<T>> {
   public String toString() {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < size; i++) {
-      sb.append(heap[getIndexAt(i)].data);
+      sb.append(heap[i].data);
       if (i < size - 1) {
         sb.append(", ");
       }
@@ -92,37 +95,21 @@ public class PriorityQueue<T extends Comparable<T>> {
     return sb.toString();
   }
 
-  public int size() {
-    return size;
-  }
-
-  protected int getIndexAtLast() {
-    return getIndexAt(size - 1);
-  }
-
-  protected int getNextAvailableIndex() {
-    return getIndexAt(size);
-  }
-  
-  protected int getIndexAt(int i) {
-    return i + offset;
-  }
-
   protected void percolateUp(int index) {
-    while (index / 2 > 0) {
-      int parentIndex = getParentIndex(index);
+    while (true) {
+      int parentIndex  = getParentIndex(index);
       if (heap[index].priority < heap[parentIndex].priority) {
         swap(index, parentIndex);
       }
       else {
         return;
       }
-      index /= 2;
+      index = getParentIndex(index);
     }
   }
 
   protected void percolateDown(int index) {
-    while (index * 2 <= size()) {
+    while (index * 2 + 1 < size) {
       int minChildIndex = getMinChildIndex(index);
       if (heap[minChildIndex].priority < heap[index].priority) {
         swap(index, minChildIndex);
@@ -140,23 +127,27 @@ public class PriorityQueue<T extends Comparable<T>> {
     heap[indexB] = temp;
   }
 
+  protected int getIndexAtLast() {
+    return size - 1;
+  }
+
   protected int getParentIndex(int i) {
-    return i / 2;
+    return (i - 1) / 2;
   }
 
   protected int getLeftChildIndex(int i) {
-    return i * 2;
+    return i * 2 + 1;
   }
 
   protected int getRightChildIndex(int i) {
-    return i * 2 + 1;
+    return i * 2 + 2;
   }
 
   protected int getMinChildIndex(int index) {
     int left = getLeftChildIndex(index);
     int right = getRightChildIndex(index);
 
-    if (right >= getIndexAtLast()) {
+    if (right > getIndexAtLast()) {
       return left;
     }
     if (heap[left].priority < heap[right].priority) {
@@ -164,17 +155,6 @@ public class PriorityQueue<T extends Comparable<T>> {
     }
     else {
       return right;
-    }
-  }
-
-  protected void ensureCapacity() {
-    if (size() + 1 >= capacity) {
-      capacity *= GROWTH_FACTOR;
-      HeapNode[] newHeap = new HeapNode[capacity];
-      for (int i = 0; i < heap.length; i++) {
-        newHeap[i] = heap[i];
-      }
-      heap = newHeap;
     }
   }
 }
